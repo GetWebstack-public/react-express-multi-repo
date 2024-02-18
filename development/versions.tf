901bac8cd4dab7cf68eaf3c4bbd106d8d2b36e99
Vlad Nistorica <vlad.nistorica@gmail.com>
2024-02-18 12:39:32 +0100

    Added gitignore

--- development/versions.tf
+++ development/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     minikube = {
       source  = "scott-the-programmer/minikube"
-      version = "0.3.5"
+      version = "0.3.10"
     }
   }
 }
--- provision-infrastructure.sh
+++ provision-infrastructure.sh
@@ -1,7 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash --pure
+#! nix-shell -p bash google-cloud-sdk opentofu minikube docker
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/6ed8f0348cf12a3d4ed2c7c535a21e3cdbd850f5.tar.gz
+
 set -euo pipefail
 
+# Can be either production / development / prod / dev
 ENVIRONMENT=$1
+
+# If ENVIRONMENT is prod or dev, set it to production or development
+if [ "$ENVIRONMENT" == "prod" ]; then
+    ENVIRONMENT="production"
+elif [ "$ENVIRONMENT" == "dev" ]; then
+    ENVIRONMENT="development"
+fi
 
 # If first argument is not set or it's not production or development, exit
 if [ -z "$ENVIRONMENT" ] || [ "$ENVIRONMENT" != "production" ] && [ "$ENVIRONMENT" != "development" ]; then
@@ -11,33 +23,39 @@ fi
 
 cd $ENVIRONMENT
 
-# Pre-terraform apply
-# if [ "$ENVIRONMENT" == "production" ]; then
-#     {
-#         gcloud auth application-default login
-#     } || {
-#         cd ..
-#     }
-# fi
+# Pre-provision apply
+if [ "$ENVIRONMENT" == "production" ]; then
+    {
+        gcloud auth application-default login
+    } || {
+        cd ..
+    }
+    elif [ "$ENVIRONMENT" == "development" ]; then
+    {
+        docker context use default
+    } || {
+        cd ..
+    }
+fi
 
 {
-    terraform workspace select -or-create $ENVIRONMENT &&
-    terraform init &&
-    terraform apply
+    tofu workspace select -or-create $ENVIRONMENT &&
+    tofu init &&
+    tofu apply
 } || {
     cd ..
 }
 
-# Post-terraform apply
+# Post-provision apply
 if [ "$ENVIRONMENT" == "production" ]; then
     {
-        gcloud container clusters get-credentials $(terraform output -raw kubernetes_cluster_name) --region $(terraform output -raw region) --project $(terraform output -raw project_id)
+        gcloud container clusters get-credentials $(tofu output -raw kubernetes_cluster_name) --region $(tofu output -raw region) --project $(tofu output -raw project_id)
     } || {
         cd ..
     }
 elif [ "$ENVIRONMENT" == "development" ]; then
     {
-        minikube profile  $(terraform output -raw cluster_name)
+        minikube profile  $(tofu output -raw cluster_name)
     } || {
         cd ..
     }
